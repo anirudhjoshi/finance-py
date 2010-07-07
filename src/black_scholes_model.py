dec0ceccbd106989ab69177bf9aea7a4a4c16449
Ray Salem <raysalem@gmail.com>
2010-07-07 13:54:27 +0000

    minor

--- src/black_scholes_model.py
+++ src/black_scholes_model.py
@@ -21,22 +21,40 @@ from numpy import *
 from scipy import *
 
 def erf(x):
-	return 1 - stats.erfc(x)
+#	return 1 - stats.erfc(x)
+    # save the sign of x
+    sign = 1
+    if x < 0: 
+        sign = -1
+    x = abs(x)
+
+    # constants
+    a1 =  0.254829592
+    a2 = -0.284496736
+    a3 =  1.421413741
+    a4 = -1.453152027
+    a5 =  1.061405429
+    p  =  0.3275911
+
+    # A&S formula 7.1.26
+    t = 1.0/(1.0 + p*x)
+    y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*math.exp(-x*x)
+    return sign*y # erf(-x) = -erf(x)
 
 # Stock price
-p = 45;
+p = 132.26;
 
 # Strike price
-s = 50;
+s = 135;
 
 # Time remaining to expiration (unit is days)
-t = 60;
+t = 10;
 
 # Current Risk free interest rate. (unit is percentage) 
-r = 10;
+r = 1;
 
 #Volatility measured by annual standard deviation (unit is percentage)
-v = 30;
+v = 39.79;
 
 
 # normalize
@@ -53,3 +71,4 @@ N2 = 1/2 * (1 + erf(d2/sqrt(2)));
 price = p * N1 - s * exp(-r * t) * N2;
 
 
+print price
