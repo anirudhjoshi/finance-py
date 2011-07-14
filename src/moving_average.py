c1ee65b7f8af1812e4fc7b1688c909fc56f51c7b
Ray Salem <raysalem@gmail.com>
2011-07-14 23:31:10 +0000

    Fixed a minor typo for stoch, and improved moving average interface

--- src/moving_average.py
+++ src/moving_average.py
@@ -1,7 +1,7 @@
 ################################################################################
 # Copyright (C)  2010 Ray M. Salem
 # http://code.google.com/p/finance-py/
-# Distributed under the GPL license Version 3.0 ( See accompanying file 
+# Distributed under the GPL license Version 3.0 ( See accompanying file
 # License_ or copy at http://code.google.com/p/finance-py/LICENSE)
 ################################################################################
 
@@ -12,12 +12,12 @@ from scipy import *
 def ema(input,length):
     if __debug__ and input.__len__() == 0:
       print "moving_average:ema -> input length is equal to 0"
-        
+
     N = input.__len__();
     output = zeros(N);
     output[0] = input[0];
     sum = input[0];
-    
+
     sf1 = 2.0/ (length+1.0);
     sf2 = 1.0-sf1;
     indxO = 1;
@@ -27,11 +27,19 @@ def ema(input,length):
         indxO = indxO +1;
     return output
 
-# moving avergage
-def ma(input, length):
+# Simple Moving average
+def sma(input, length):
     weights = ones(length) / length
     output = convolve(input, weights, mode='full')[:len(input)]
     return output;
-# Moving average
-def sma(input, length):
-    return ma(input, length)
+
+# moving avergage, allows for a specification of the type
+def ma(input, length, type):
+    if(type == "sma"):
+         return sma(input, length);
+    elif(type == "ema"):
+         return ema(input, length);
+    else:
+         print ("incorrect moving average type specififed")
+
+
--- src/stochastic_oscillator.py
+++ src/stochastic_oscillator.py
@@ -1,7 +1,7 @@
 ################################################################################
 # Copyright (C)  2010 Ray M. Salem
 # http://code.google.com/p/finance-py/
-# Distributed under the GPL license Version 3.0 ( See accompanying file 
+# Distributed under the GPL license Version 3.0 ( See accompanying file
 # License_ or copy at http://code.google.com/p/finance-py/LICENSE)
 ################################################################################
 
@@ -33,13 +33,13 @@ def stoc_osc(high, low, close, k_periods, k_slow_periods, d_periods, d_method):
         min_low     = min(low[0:index+1]);
         numerator[index]   = abs(close[index] - min_low);
         denominator[index] = abs(max_high - min_low); # abs to handle negative values
-        
+
     for index in arange(k_periods-1,N):
         max_high    = max(high[index-k_periods+1:index+1]);
         min_low     = min(low[index-k_periods+1:index+1]);
         numerator[index]   = close[index] - min_low;
         denominator[index] = max_high - min_low;
-        
+
     # %K Slowing periods, sum over last slowing periods (moving average work since we are doing a division)
     num = moving_average.sma(numerator, k_slow_periods);
     den = moving_average.sma(denominator, k_slow_periods);
@@ -49,10 +49,7 @@ def stoc_osc(high, low, close, k_periods, k_slow_periods, d_periods, d_method):
         else:
             per_k[index] = num[index] / den[index] * 100;
 
-    if(d_method == "ema"):
-        per_d = moving_average.ema(per_k, d_periods);
-    elif(d_method == "sma"):
-        per_d = moving_average.ema(per_k, d_periods);
-    else:
-        print("incorrect moving average method specified,( " + d_method + " ) is not supporteD");
+
+    per_d = moving_average.ma(per_k, d_periods, d_method);
+
     return [per_k, per_d];
