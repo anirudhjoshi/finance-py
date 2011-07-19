f957b0ff563b16e454c0a24de66886c090c6ba40
Ray Salem <raysalem@gmail.com>
2011-07-19 06:36:00 +0000

    prep-run is now functional,

--- util/prep_run.py
+++ util/prep_run.py
@@ -1,9 +1,32 @@
 from numpy import *
 from scipy import *
 import csv
+import sys
+import os
 from xml.dom.minidom import Document
 
-with open('example.csv', 'r') as f:
+##
+#	1 -argument
+#		specify input file name
+# 	2- argument 
+#		* path
+#		* input file name
+#
+##
+
+# True --> each test case will get a unique directory, but output xml will allways be called parameters.xml
+individualTestDir = True;
+workPath = ""
+if (sys.argv.__len__() == 2):
+	fileName = sys.argv[1];	
+elif (sys.argv.__len__() == 3):
+	workPath = sys.argv[1];
+	fileName = sys.argv[2];	
+else:
+  print "Must provide symbol and days going back.  for example: yhoo 1000"
+  exit()
+
+with open(workPath + "//" + fileName, 'r') as f:
 	reader = csv.reader(f)
 	rowIndex =  0;
 	for row in reader:
@@ -23,6 +46,9 @@ with open('example.csv', 'r') as f:
 			# Create the <main> base element
 			top = doc.createElement("dvg_sns")
 			top.setAttribute("testName", cols[0])
+			if(individualTestDir  == True):
+				workPath = workPath + cols[0] 
+				os.mkdir(workPath)
 			doc.appendChild(top)
 			for index in range(1,N):
 				print "\t%s = %s" % ( header[index], cols[index])
@@ -30,8 +56,10 @@ with open('example.csv', 'r') as f:
 				parameter.setAttribute("name", header[index])
 				parameter.setAttribute("value", cols[index])
 				top.appendChild(parameter)
-			
-			f = open(cols[0] + ".xml",'w');
+			if(individualTestDir ==True):
+				f = open(workPath + "//parameters.xml",'w');
+			else:
+				f = open(workPath + "//" + cols[0] + ".xml",'w');
 			doc.writexml(f);
 			f.close()
 		
