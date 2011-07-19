776f4beadd3fde3bdb05353483fcfaca8c52bf72
Ray Salem <raysalem@gmail.com>
2011-07-19 05:45:07 +0000

    add util function to convert .csv file into individual test case for back end testing

--- util/prep_run.py
+++ util/prep_run.py
@@ -1,6 +1,8 @@
 from numpy import *
 from scipy import *
 import csv
+from xml.dom.minidom import Document
+
 with open('example.csv', 'r') as f:
 	reader = csv.reader(f)
 	rowIndex =  0;
@@ -11,9 +13,27 @@ with open('example.csv', 'r') as f:
 		else:
 			cols = row#.split(',')
 			print cols[0]
+			
+			# Create the minidom document
+			doc = Document()
+			cc1 = doc.createComment("license:RMS Investment 2011")
+			cc2 = doc.createComment("strictly confidential. Propietary Information")
+			doc.appendChild(cc1)
+			doc.appendChild(cc2)
+			# Create the <main> base element
+			top = doc.createElement("dvg_sns")
+			top.setAttribute("testName", cols[0])
+			doc.appendChild(top)
 			for index in range(1,N):
-				print "%s = %s" % ( header[index], cols[index])
-				
+				print "\t%s = %s" % ( header[index], cols[index])
+				parameter = doc.createElement("parameter")
+				parameter.setAttribute("name", header[index])
+				parameter.setAttribute("value", cols[index])
+				top.appendChild(parameter)
+			
+			f = open(cols[0] + ".xml",'w');
+			doc.writexml(f);
+			f.close()
 		
 		
 		
